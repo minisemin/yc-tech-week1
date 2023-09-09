779c8c1be5283891242a59d50f9052269ba64c08
minisemin <minisemin@yonsei.ac.kr>
2023-09-09 17:21:42 +0900

    added TestContoller.java

---
+++ sns/src/main/java/com/example/sns/TestController.java
@@ -0,0 +1,17 @@
+package com.example.sns;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class TestController {
+  @GetMapping("/hello")
+  public ResponseEntity<String> hello(
+      @RequestParam(value = "name", defaultValue = "World") String name) {
+    return ResponseEntity.ok("Hello " + name + " !");
+  }
+}
